df2d79f0ed66ee1f8337bb742b65caf009ba54e9
Pavel Machuca <pavelmachuca@hotmail.com>
2019-01-27 13:13:36 -0500

    works to send a set string for now, no interpolation

--- chromeext/background.js
+++ chromeext/background.js
@@ -10,7 +10,7 @@ let contentTabId;
 
 //gets tabID from content
 chrome.runtime.onMessage.addListener(function(msg, sender) {
-  if (msg.from == "content") {
+  if (msg.from == 'content') {
     contentTabId = sender.tab.id;
     console.log(contentTabId);
   }
@@ -18,26 +18,33 @@ chrome.runtime.onMessage.addListener(function(msg, sender) {
 
 //takes received data back from content and posts to db
 const sendToServer = function(data) {
-  //******SERVER NAME HERE!!!! ****/
-  fetch("http://localhost:1337" /*change dummy server name please!*/, {
-    method: "Post",
+  console.log('data is \n\n', data);
+  fetch('http://localhost:4000/?', {
+    credentials: 'include',
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
+      accept: 'application/json',
+      'accept-language': 'en-US,en;q=0.9,it;q=0.8,la;q=0.7',
+      'content-type': 'application/json'
     },
-    body: data
+    referrer: 'http://localhost:4000/',
+    referrerPolicy: 'origin',
+    body:
+      '{"query":"mutation{\\n  addArticle(name:\\"123\\"){\\n  \\tname\\n\\t}\\n}","variables":null}',
+    method: 'POST',
+    mode: 'cors'
   }).then(response => {
-    console.log(response);
+    console.log('res to query\n\n\n', response);
   });
 };
 
 //on click triggers function in content to grab dom
 chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.sendMessage(contentTabId, { method: "triggerDOM" });
+  chrome.tabs.sendMessage(contentTabId, { method: 'triggerDOM' });
 });
 
 //receives DOM from content
 chrome.runtime.onMessage.addListener(function(newmsg) {
-  if (newmsg.title === "fullDOM") {
+  if (newmsg.title == 'fullDOM') {
     sendToServer(newmsg.content);
   }
 });
