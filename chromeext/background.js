68aa40bd025ec7179afb6c76d062274d707b96c8
Pavel Machuca <pavelmachuca@hotmail.com>
2019-01-28 01:57:51 -0500

    query passes variables

--- chromeext/background.js
+++ chromeext/background.js
@@ -1,16 +1,9 @@
-//gets us the url and title
-
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   console.log("url", tab.url);
-//   console.log("title", tab.title);
-// });
-
 //tab id
 let contentTabId;
 
 //gets tabID from content
 chrome.runtime.onMessage.addListener(function(msg, sender) {
-  if (msg.from === "content") {
+  if (msg.from === 'content') {
     contentTabId = sender.tab.id;
     console.log(contentTabId);
   }
@@ -18,20 +11,29 @@ chrome.runtime.onMessage.addListener(function(msg, sender) {
 
 //takes received data back from content and posts to db
 const sendToServer = function(data) {
-  console.log('data is \n\n', data);
-  fetch('http://localhost:4000/?', {
-    credentials: 'include',
-    headers: {
-      accept: 'application/json',
-      'accept-language': 'en-US,en;q=0.9,it;q=0.8,la;q=0.7',
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    referrer: 'http://localhost:4000/',
-    referrerPolicy: 'origin',
-    body:
-      '{"query":"mutation{\\naddArticle(title:\\"from chrome ext\\" content:\\"some content\\"){\\n  title\\n    content\\n\\t}\\n}","variables":null}',
+  const title = data[0];
+  const content = data[1];
+
+  const queryJSON = JSON.stringify({
+    query: `
+        mutation($title: String $content: String) {
+          addArticle(title: $title, content: $content){
+            title
+          }
+        }
+    `,
+    variables: {
+      title,
+      content
+    }
+  });
+  fetch('http://localhost:4000/', {
     method: 'POST',
-    mode: 'cors'
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: queryJSON
   }).then(response => {
     console.log('res to query\n\n\n', response);
   });
@@ -39,25 +41,10 @@ const sendToServer = function(data) {
 
 //on click triggers function in content to grab dom
 chrome.browserAction.onClicked.addListener(function() {
-  console.log('arguments \n\n', [...arguments][0]);
   contentTabId = [...arguments][0].id;
-  fetch('http://localhost:4000/?', {
-    credentials: 'include',
-    headers: {
-      accept: 'application/json',
-      'accept-language': 'en-US,en;q=0.9,it;q=0.8,la;q=0.7',
-      'content-type': 'application/json'
-    },
-    referrer: 'http://localhost:4000/',
-    referrerPolicy: 'origin',
-    body:
-      '{"query":"mutation{\\naddArticle(title:\\"from chrome ext\\" content:\\"some content\\"){\\n  title\\n    content\\n\\t}\\n}","variables":null}',
-    method: 'POST',
-    mode: 'cors'
-  }).then(response => {
-    console.log('res to query\n\n\n', response);
-  });
-  // 'Content-Type': 'application/x-www-form-urlencoded'
+  const title = [...arguments][0].title;
+  const url = [...arguments][0].url;
+  sendToServer([title, url]);
 
   chrome.tabs.sendMessage(contentTabId, { method: 'triggerDOM' });
 });
